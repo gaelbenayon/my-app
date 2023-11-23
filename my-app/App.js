63cb9474399e2ee1df23e32d4a395a7b43345138
gaelbenayon <gaelbenayon@gmail.com>
2023-11-22 23:27:01 -0300

    Update App.js

--- my-app/App.js
+++ my-app/App.js
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, Image } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hola, Coder!</Text>
+      <Text style={styles.title}>CAMPANA REPARA</Text>
+      <Image style={styles.mainImage} source={{uri:'https://upload.wikimedia.org/wikipedia/commons/7/74/Municipalidad_de_Campana.jpg'}}/>
+      <Button title='Hacer un reclamo'></Button>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +19,14 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  title: {
+    fontSize: 30,
+    fontWeight: 'bold',
+    color: 'purple',
+    padding: 15,
+  },
+  mainImage: {
+    width: '80%',
+    height: '50%',
+  }
 });
