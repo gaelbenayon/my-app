2a1f61279dac40a89463eebfa780aebb22b212db
gaelbenayon <gaelbenayon@gmail.com>
2023-11-27 22:59:42 -0300

    FlatList, Modal

--- my-app/App.js
+++ my-app/App.js
@@ -1,32 +1,92 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button, Image } from 'react-native';
+import { StyleSheet, Text, View, Button, Image, TextInput, FlatList, Modal } from 'react-native';
+import { useState } from 'react';
 
 export default function App() {
+
+  const [newItem,setNewItem] = useState("");
+  const [itemList,setItemList] = useState([]);
+  const [itemSelectedToDelete,setItemSelectedToDelete] = useState({});
+  const [modalVisible,setModalVisible] = useState(false);
+
+  const onChangeTextHandler = (text) => {
+    setNewItem(text);
+  }
+
+  const addItemToListHandler = () => {
+    setItemList(prevState => [...prevState,{id: Math.random(),value: newItem}]);
+    setNewItem("");
+  }
+
+  const renderItemList = ({item}) => {
+    return (
+      <View style={styles.itemList}>
+        <Text>{item.value}</Text>
+        <Button title='X'></Button>
+      </View>
+    )
+  }
+
   return (
+    <>
     <View style={styles.container}>
-      <Text style={styles.title}>CAMPANA REPARA</Text>
-      <Image style={styles.mainImage} source={{uri:'https://upload.wikimedia.org/wikipedia/commons/7/74/Municipalidad_de_Campana.jpg'}}/>
-      <Button title='Hacer un reclamo'></Button>
+      <View style={styles.inputContainer}>
+        <TextInput onChangeText={onChangeTextHandler} style={styles.textInput} value={newItem} placeholder='Ingrese su tarea...'/>
+        <Button onPress={addItemToListHandler} title='Añadir'></Button>
+      </View>
+      {/* <View>
+        {itemList.map(item=><View key={item.id}><Text>{item.value}</Text></View>)}
+      </View> */}
+      
+        <FlatList style={styles.flatList} 
+        data={itemList} 
+        renderItem={renderItemList} 
+        keyExtractor={item=>item.id}
+        />
+      
       <StatusBar style="auto" />
     </View>
+    <Modal animationType='slide' visible={modalVisible}>
+      <View style={styles.modalMessageContainer}>
+        <Text>Se eliminará: </Text>
+        <Text>{itemSelectedToDelete.value}</Text>
+      </View>
+      <View style={styles.modalButtonContainer}>
+        <Button title="Cancelar"></Button>
+        <Button title="Eliminar"></Button>
+      </View>
+    </Modal>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    padding: 50,
+  },
+  inputContainer: {
     alignItems: 'center',
     justifyContent: 'center',
   },
-  title: {
-    fontSize: 30,
-    fontWeight: 'bold',
-    color: 'purple',
+  textInput: {
+    fontSize: 20,
     padding: 15,
   },
-  mainImage: {
-    width: '80%',
-    height: '50%',
-  }
+  flatList: {
+    backgroundColor: "yellow"
+  },
+  itemList: {
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  modalMessageContainer: {
+    marginTop: 50,
+    alignItems: "center",
+  },
+  modalButtonContainer:{
+    flexDirection:"row",
+    justifyContent: "space-evenly",
+    paddingTop: 20,
+  },
 });
